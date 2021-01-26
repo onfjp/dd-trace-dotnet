40f4ef007a6519833b12c458e11d1f4f6ded4d5a
Andrew Lock <andrew.lock@datadoghq.com>
2021-01-26 10:01:45 +0000

    Fix HttpClientHandler CallTarget method signature

--- src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/HttpClientHandler/HttpClientHandlerSyncIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/HttpClientHandler/HttpClientHandlerSyncIntegration.cs
@@ -46,10 +46,11 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.HttpClientHandler
         /// <param name="exception">Exception instance in case the original code threw an exception.</param>
         /// <param name="state">Calltarget state value</param>
         /// <returns>A response value, in an async scenario will be T of Task of T</returns>
-        public static TResponse OnMethodEnd<TTarget, TResponse>(TTarget instance, TResponse responseMessage, Exception exception, CallTargetState state)
+        public static CallTargetReturn<TResponse> OnMethodEnd<TTarget, TResponse>(TTarget instance, TResponse responseMessage, Exception exception, CallTargetState state)
             where TResponse : IHttpResponseMessage
         {
-            return HttpClientHandlerCommon.OnMethodEnd(instance, responseMessage, exception, state);
+            var response = HttpClientHandlerCommon.OnMethodEnd(instance, responseMessage, exception, state);
+            return new CallTargetReturn<TResponse>(response);
         }
     }
 }
